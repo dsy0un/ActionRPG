a0fb137465c92c31a2d929016deaa0dc25c22f06
dsy0un <jeadan1010@gmail.com>
2024-09-12 20:42:49 +0900

    2

---
+++ ActionRPG_Main/Assets/52SpecialEffectPack/Animation&Script/csDestroyEffect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csDestroyEffect : MonoBehaviour 
+{
+    private void Start()
+    {
+        Destroy(gameObject, 3.0f);
+    }
+
+    void Update()
+    {
+	    
+	}
+}
---
+++ ActionRPG_Main/Assets/52SpecialEffectPack/Animation&Script/csParticleMove.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csParticleMove : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    public GameObject explosion;
+    public float expRange;
+
+    bool expSign = true;
+
+    float magicDamage = 40.0f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, expRange);
+    }
+
+    void Update () {
+        transform.Translate(Vector3.forward * speed);
+        MagicHit();
+	}
+
+    void MagicHit()
+    {
+        int layerMask = 1 << 7;
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, expRange, Vector3.up, 0, layerMask);
+        if (hits.Length > 0)
+        {
+            foreach (RaycastHit hit in hits)
+            {
+                hit.transform.SendMessage("MonsterHPDown", magicDamage); // 충돌한 대상(몬스터)에게 MonsterHPDown 함수를 찾아 호출
+                GameObject exp = Instantiate(explosion, hit.transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.transform.gameObject.layer == 7)
+    //    {
+    //        if (expSign)
+    //        {
+    //            other.transform.SendMessage("MonsterHPDown", magicDamage); // 충돌한 대상(몬스터)에게 MonsterHPDown 함수를 찾아 호출
+    //            GameObject exp = Instantiate(explosion, other.transform.position, Quaternion.identity);
+    //            Destroy(gameObject);
+    //        }
+    //    }
+    //}
+}
